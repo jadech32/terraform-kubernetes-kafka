5bfcde40f5294aee68f2c119ece9e1f379a2f65f
Jakrarat Chunnananda <jade.jch@gmail.com>
2020-10-21 15:29:11 -0700

    Add more environment variables

--- main.tf
+++ main.tf
@@ -332,6 +332,22 @@ resource "kubernetes_stateful_set" "kafka" {
             value = "${var.kafka_name}-zookeeper:2181"
           }
           env {
+            name  = "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP"
+            value = "INTERNAL_PLAINTEXT:PLAINTEXT,EXTERNAL_PLAINTEXT:PLAINTEXT"
+          }
+          env {
+            name  = "KAFKA_ADVERTISED_LISTENERS"
+            value = "INTERNAL_PLAINTEXT://${var.kafka_name}:9092"
+          }
+          env {
+            name  = "KAFKA_LISTENERS"
+            value = "INTERNAL_PLAINTEXT://0.0.0.0:9092"
+          }
+          env {
+            name  = "KAFKA_INTER_BROKER_LISTENER_NAME"
+            value = "INTERNAL_PLAINTEXT"
+          }
+          env {
             name  = "KAFKA_LOG_DIRS"
             value = "/opt/kafka/data/logs"
           }
